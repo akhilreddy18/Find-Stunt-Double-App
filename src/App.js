303c50ed4bdde6d28b0e407f0a60b17dd1978cf0
Akhil <akhil.pannala@byon8.com>
2020-07-07 15:06:00 +0100

    build static html and css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     The app will be coming up.
+    <div className="Entire">
+      <div className="App">
+        <div className="header">
+          <img src="/images/logo.jpg" height="80"></img>
+          <h3>Position: Stunt double</h3>
+        </div>
+        <div className="card">
+          <div className="person-photo">
+            <img src="./images/keanu.jpg"></img>
+          </div>
+          <div className="person-name">
+            <p>Akhil</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

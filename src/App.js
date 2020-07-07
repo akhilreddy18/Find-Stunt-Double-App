d8a1301614cdf0f849b6b319284dea4cf3012a77
Akhil <akhil.pannala@byon8.com>
2020-07-07 15:34:30 +0100

    add like and dislike buttons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function Dislike(){
+    console.log("clicked");
+  }
+
+  function Like(){
+    console.log("clicked");
+  }
   return (
     <div className="Entire">
       <div className="App">
         <div className="header">
-          <img src="/images/logo.jpg" height="80"></img>
+          <img src="/images/logo.jpg" height="80" alt="logo"></img>
           <h3>Position: Stunt double</h3>
         </div>
         <div className="card">
           <div className="person-photo">
-            <img src="./images/keanu.jpg"></img>
+            <img src="./images/keanu.jpg" alt="keanu"></img>
           </div>
           <div className="person-name">
             <p>Akhil</p>
           </div>
         </div>
+      </div>
+      <div className="buttons">
+      <input type="image" name="submit" src="./images/dislike.png" width="40px" alt="Dislike button" onClick={Dislike}></input>
+      <input type="image" name="submit" src="./images/like.png" width="50px" alt="Like button" onClick={Like}></input>
       </div>
     </div>
   );
